f7ff99aeb5ba62db627659aab7b6c1dd67df1783
Munwar_Ali <126280146+MunwarAli12@users.noreply.github.com>
2023-04-17 20:19:19 +0530

    Update GithubactionsCiCdApplication.java

--- src/main/java/com/munwarcicd/githubactionscicd/GithubactionsCiCdApplication.java
+++ src/main/java/com/munwarcicd/githubactionscicd/GithubactionsCiCdApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class GithubactionsCiCdApplication {
 	@GetMapping("/welcome")
 	public String welcome(){
-		return "Welcome to Munwarportfolio !";
+		return "Welcome to Munwarjoddddddd !";
 	}
 
 	public static void main(String[] args) {
