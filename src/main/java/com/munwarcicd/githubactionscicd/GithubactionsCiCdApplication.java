cb4544a1c77e8eb83dbf878e744cdfb5c47ac807
Munwar_Ali <126280146+MunwarAli12@users.noreply.github.com>
2023-07-06 14:34:07 +0530

    Update GithubactionsCiCdApplication.java

--- src/main/java/com/munwarcicd/githubactionscicd/GithubactionsCiCdApplication.java
+++ src/main/java/com/munwarcicd/githubactionscicd/GithubactionsCiCdApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class GithubactionsCiCdApplication {
 	@GetMapping("/welcome")
 	public String welcome(){
-		return "CHECKING WHETHER THE PIPELING IS GENERATING OR NOT !!!!!!";
+		return "CHECKING WHETHER THE PIPELING IS GENERATING OR NOT !!!!!!    chjdbchdcbdcdihcbaciabcsichbciahbcadsihcbadcihdbfwdihcbdwfhiwbfih	wlrbvwihvbwdivhbwvhwhebwejfbwefvjdwnvdwjvnwv;ljdvn;dslvn";
 	}
 
 	public static void main(String[] args) {
