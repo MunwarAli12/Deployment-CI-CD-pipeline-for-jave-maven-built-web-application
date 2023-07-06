512a883a442bc7e111526995015686504cec86aa
Munwar_Ali <126280146+MunwarAli12@users.noreply.github.com>
2023-07-06 21:45:46 +0530

    Update GithubactionsCiCdApplication.java

--- src/main/java/com/munwarcicd/githubactionscicd/GithubactionsCiCdApplication.java
+++ src/main/java/com/munwarcicd/githubactionscicd/GithubactionsCiCdApplication.java
@@ -12,42 +12,7 @@ public class GithubactionsCiCdApplication {
 	public String welcome(){
 		return "CHECKING WHETHER THE PIPELING IS GENERATING OR NOT !!!!!!    chjdbchdcbdcdihcbaciabcsichbciahbcadsihcbadcihdbfwdihcbdwfhiwbfih	wlrbvwihvbwdivhbwvhwhebwejfbwefvjdwnvdwjvnwv;ljdvn;dslvn";
 	}
-	@PostMapping("/users")
-public String createUser(@RequestBody User user) {
-    // Basic logic to create a user
-    if (user.getUsername() != null && user.getEmail() != null) {
-        // Save the user to the database or perform any other required operations
-        return "User created successfully: " + user.getUsername();
-    } else {
-        return "Failed to create user. Invalid data provided.";
-    }
-}
-
-@GetMapping("/users/{id}")
-public String getUser(@PathVariable("id") int id) {
-    // Basic logic to get user by ID
-    // Retrieve the user from the database or any other data source
-    User user = userRepository.findById(id);
-    if (user != null) {
-        return "User with ID " + id + " retrieved. Username: " + user.getUsername();
-    } else {
-        return "User with ID " + id + " not found.";
-    }
-}
-
-@DeleteMapping("/users/{id}")
-public String deleteUser(@PathVariable("id") int id) {
-    // Basic logic to delete user by ID
-    // Delete the user from the database or any other data source
-    boolean isDeleted = userRepository.deleteById(id);
-    if (isDeleted) {
-        return "User with ID " + id + " deleted successfully.";
-    } else {
-        return "Failed to delete user. User with ID " + id + " not found.";
-    }
-}
-
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GithubactionsCiCdApplication.class, args);
 	}
