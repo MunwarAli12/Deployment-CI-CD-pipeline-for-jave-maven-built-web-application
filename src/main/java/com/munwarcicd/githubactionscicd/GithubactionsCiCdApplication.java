13f83512dbc6b4eb17970bbf92b641cc5714336c
Munwar Ali <126280146+MunwarAli12@users.noreply.github.com>
2024-01-25 20:12:21 +0530

    Update GithubactionsCiCdApplication.java

--- src/main/java/com/munwarcicd/githubactionscicd/GithubactionsCiCdApplication.java
+++ src/main/java/com/munwarcicd/githubactionscicd/GithubactionsCiCdApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.HashMap;
+import java.util.Map;
+
 
 @SpringBootApplication
 @RestController
